df3b6c1899b105819a3ee1c7c50ea8b36ad7b3f2
István Gansperger <gansperger@gmail.com>
2015-07-23 15:40:51 +0200

    Replaced old api with the ground of a new one

--- WebSharper.Charting.Test/Client.fs
+++ WebSharper.Charting.Test/Client.fs
@@ -4,52 +4,54 @@ open WebSharper
 open WebSharper.JavaScript
 open WebSharper.Html.Client
 open WebSharper.Charting
+open WebSharper.Charting.Sandbox
 open IntelliFactory.Reactive
 
 [<JavaScript>]
 module Client =
 
+    let insert (el : Element) =
+        el.AppendTo "entry"
+
     let Main =
+        let renderer = 
+            Renderers.ChartJs.LineChartRenderer({ Width = 500; Height = 500 })
+
+        [ for x in 1.0 .. 10.0 -> (string x, x ** 2.0) ]
+        |> Chart.Line
+        |> renderer.Render
+        |> insert
+
+        // live chart
+
+        let liveRenderer =
+            Renderers.ChartJs.Live.LineChartRenderer({ Width = 500; Height = 500 }, 10)
+
         let stream = BufferedStream<float>.New 40
+        
+        stream
+        |> LiveChart.Line
+        |> liveRenderer.Render
+        |> insert
 
-        let s = 
-            Reactive.Select
-            <| Reactive.Aggregate stream (0, 0.0) (fun (s, _) c -> (s + 1, c)) 
-            <| fun (a, b) -> (string a, b)
-
-        let avg =
-            Reactive.Select
-            <| Reactive.Aggregate stream (0, 0.0) (fun (s, e) c -> 
-                let fs = float s
-                (s + 1, (e * fs + c) / (fs + 1.)))
-            <| fun (a, b) -> (string a, b)
+        Reactive.Select
+        <| Reactive.Aggregate stream (0, 0.0) (fun (s, e) c -> 
+            let fs = float s
+            (s + 1, (e * fs + c) / (fs + 1.)))
+        <| fun (a, b) -> (string a, b)
+        |> LiveChart.Line
+        |> liveRenderer.Render
+        |> insert
 
         let rand = System.Random()
-
         async {
-            for i in 1 .. 20 do
-                let iv = rand.Next(300, 2500)
+            while true do
+                let iv = rand.Next(600, 1500)
                 do! Async.Sleep iv
                 stream.Trigger <| (rand.NextDouble() * 300.)
         }
         |> Async.Start
+
+
         
-        
-        Div [
-            Chart.Line s
-            |> Chart.WithDimension (600, 300)
-            |> Chart.WithWindow 5
-        ] -< [
-            Br []
-        ]
-        |>! OnAfterRender (fun el ->
-            async {
-                do! Async.Sleep 3000
-                let ch = 
-                    Chart.Line avg
-                    |> Chart.WithDimension (600, 300)
-                el.Append ch
-            }
-            |> Async.Start
-        )
-        |> fun el -> el.AppendTo "entry"
+
