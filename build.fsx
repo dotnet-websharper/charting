fa8907d604c1ef3ecb23df3a4f0f8f5925a96a33
Loic Denuziere <loic@denuziere.net>
2017-10-10 14:42:01 +0200

    Convert to FAKE+Paket+dotnet

--- build.fsx
+++ build.fsx
@@ -1,47 +1,17 @@
-#load "tools/includes.fsx"
+#load "paket-files/build/intellifactory/websharper/tools/WebSharper.Fake.fsx"
+open Fake
+open WebSharper.Fake
 
-open IntelliFactory.Build
+let targets =
+    GetSemVerOf "WebSharper"
+    |> ComputeVersion
+    |> WSTargets.Default
+    |> MakeTargets
 
-let bt =
-    BuildTool().PackageId("WebSharper.Charting")
-        .VersionFrom("WebSharper")
-        .WithFramework(fun fw -> fw.Net40)
+Target "Build" DoNothing
+targets.BuildDebug ==> "Build"
 
-let main =
-    bt.WebSharper4.Library("WebSharper.Charting")
-        .SourcesFromProject()
-        .WithSourceMap()
-        .References(fun r ->
-            [ 
-                r.NuGet("WebSharper.ChartJs").Latest(true).ForceFoundVersion().Reference()
-                r.NuGet("WebSharper.Reactive").Latest(true).ForceFoundVersion().Reference()
-                r.NuGet("WebSharper.UI.Next").Latest(true).ForceFoundVersion().Reference()
-            ])
+Target "CI-Release" DoNothing
+targets.CommitPublish ==> "CI-Release"
 
-let test =
-    bt.WebSharper4.Library("WebSharper.Charting.Test")
-        .WithSourceMap()
-        .References(fun r ->
-            [ 
-                r.NuGet("WebSharper.ChartJS").Latest(true).ForceFoundVersion().Reference()
-                r.NuGet("WebSharper.Reactive").Latest(true).ForceFoundVersion().Reference()
-                r.NuGet("WebSharper.UI.Next").Latest(true).ForceFoundVersion().Reference()
-                r.Project main
-            ])
-
-bt.Solution [
-    main
-    test
-
-    bt.NuGet.CreatePackage()
-        .Configure(fun configuration ->
-            { configuration with
-                Title = Some "WebSharper.Charting"
-                LicenseUrl = Some "http://websharper.com/licensing"
-                ProjectUrl = Some "https://github.com/intellifactory/websharper.charting"
-                Description = "Chart combinator-library for WebSharper"
-                Authors = [ "IntelliFactory" ]
-                RequiresLicenseAcceptance = true })
-        .Add(main)
-]
-|> bt.Dispatch
+RunTargetOrDefault "Build"
